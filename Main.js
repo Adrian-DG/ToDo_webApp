4d0c06b3c1d064ea21efaa8dae848174915e5ed8
Adrian-DG <adrian96guilamo@gmail.com>
2020-10-11 15:26:45 -0400

    second commit

--- Main.js
+++ Main.js
@@ -3,7 +3,11 @@ const noteTitle = document.getElementById('title-control')
 const noteBody = document.getElementById('body-control')
 const noteTag = document.getElementById('tag-control')
 const noteDate = document.getElementById('date-control')
+
 const noteSubmitBtn = document.getElementById('noteBtn')
+const noteEraseBtn = document.getElementById('eraseBtn')
+
+const listItems = document.getElementsByClassName('listItem')
 
 noteSubmitBtn.addEventListener('click', function () {
     const note = {
@@ -11,17 +15,28 @@ noteSubmitBtn.addEventListener('click', function () {
         body: noteBody.value,
         tag: noteTag.value,
         date: noteDate.value.toString()
-    } 
+    }
 
     to_html(note)
     // console.log(Title: ${note.title}\nDate: ${note.date}`)
+    clearForm()
 })
+
+function clearForm() {
+    noteTitle.value = ''
+    noteBody.value = ''
+    noteDate.value = ''
+    noteTag.value = ''
+}
 
 function to_html(note) {
     const card = `  
         <div class="card">
-            <div class="title">
-                <p>${note.title}</p>
+            <div class="card-head">
+                <div class="title">
+                    <p>${note.title}</p>
+                </div>
+                <input type="button" id="eraseBtn" value="x">
             </div>
             <div class="body">
                 <p>${note.body}</p>
@@ -44,7 +59,6 @@ function render_html(card) {
     item.classList.add('listItem')
     item.innerHTML = card
     document.getElementById('todo-list').appendChild(item)
-
     get_notes()
 }
 
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# ToDo's webapp
+
+A Simple ToD's web app made with HTML/CSS and JS. 
+
+<img src="./ToDo_App.png" style="width: 300px; height: 150px">
